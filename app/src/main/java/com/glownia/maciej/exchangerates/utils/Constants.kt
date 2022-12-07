e1243a8309325aea08686cd8c204917caac073e2
Maciej Glownia <maciej.glownia@infor.com>
2022-12-07 22:54:49 +0100

    Add Constant object for constant values such as BASE_URL

---
+++ app/src/main/java/com/glownia/maciej/exchangerates/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.glownia.maciej.exchangerates.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.apilayer.com/"
+
+    // This will be automatically generated and contain API key which has been added in gradle.properties
+    // const val CLIENT_ID = BuildConfig.FIXER_ACCESS_KEY
+
+}

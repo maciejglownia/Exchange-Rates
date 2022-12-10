58966be26fb8519c9217997bdbc0350b26388f97
Maciej Glownia <maciej.glownia@infor.com>
2022-12-10 12:03:20 +0100

    Add constant values into Constants.kt object

--- app/src/main/java/com/glownia/maciej/exchangerates/utils/Constants.kt
+++ app/src/main/java/com/glownia/maciej/exchangerates/utils/Constants.kt
@@ -2,9 +2,15 @@ package com.glownia.maciej.exchangerates.utils
 
 object Constants {
 
+    // Communication
     const val BASE_URL = "https://api.apilayer.com/"
-
     // This will be automatically generated and contain API key which has been added in gradle.properties
     // const val CLIENT_ID = BuildConfig.FIXER_ACCESS_KEY
 
+    // Names to display
+    const val DAY_WORD = "Dzień"
+
+    // TAGs
+    const val MAIN_VIEW_MODEL_TAG = "MainViewModel"
+    const val EXCHANGE_RATES_FRAGMENT_TAG = "ExchangeRatesFragment"
 }

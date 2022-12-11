b56cbd8cc93c884dd5e52b8d6982cb180f11846c
Maciej Glownia <maciej.glownia@infor.com>
2022-12-11 10:33:47 +0100

    Handle passing chosen value from main screen to single screen

--- app/src/main/java/com/glownia/maciej/exchangerates/ui/fragments/SingleExchangeRateFragment.kt
+++ app/src/main/java/com/glownia/maciej/exchangerates/ui/fragments/SingleExchangeRateFragment.kt
@@ -5,33 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.glownia.maciej.exchangerates.R
+import androidx.navigation.fragment.navArgs
 import com.glownia.maciej.exchangerates.databinding.FragmentSingleExchangeRateBinding
 
 class SingleExchangeRateFragment : Fragment() {
 
+    private val args: SingleExchangeRateFragmentArgs by navArgs()
+
     private var _binding: FragmentSingleExchangeRateBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         _binding = FragmentSingleExchangeRateBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        val argumentData = args.exchangeRatesDataArgument
+        binding.apply {
+            dateTextView.text = "Date"
+            valueTextView.text = argumentData.value
         }
     }
 

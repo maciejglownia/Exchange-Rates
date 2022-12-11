e25cd2f2071b869961a8016d5995fa7028694803
Maciej Glownia <maciej.glownia@infor.com>
2022-12-11 22:07:43 +0100

    Replace API key as a string with constant

--- app/src/main/java/com/glownia/maciej/exchangerates/api/ExchangeRatesDataApi.kt
+++ app/src/main/java/com/glownia/maciej/exchangerates/api/ExchangeRatesDataApi.kt
@@ -1,11 +1,14 @@
 package com.glownia.maciej.exchangerates.api
 
 import com.glownia.maciej.exchangerates.data.ExchangeRatesData
+import com.glownia.maciej.exchangerates.utils.Constants.CLIENT_ID
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ExchangeRatesDataApi {
 
     @GET("/fixer/{date}")
-    @Headers("apikey: UJSUxXUxVns23fo5rZHlu31PM6PMubKb")
-    suspend fun fetchExchangeRatesData(@Path("date") date: String): ExchangeRatesData
+    @Headers("apikey: $CLIENT_ID")
+//    @Headers("apikey: UJKb") // this line is to test responses
+    suspend fun fetchExchangeRatesData(@Path("date") date: String): Response<ExchangeRatesData>
 }
--- app/src/main/java/com/glownia/maciej/exchangerates/utils/Constants.kt
+++ app/src/main/java/com/glownia/maciej/exchangerates/utils/Constants.kt
@@ -1,11 +1,13 @@
 package com.glownia.maciej.exchangerates.utils
 
+import com.glownia.maciej.exchangerates.BuildConfig
+
 object Constants {
 
     // Communication
     const val BASE_URL = "https://api.apilayer.com/"
     // This will be automatically generated and contain API key which has been added in gradle.properties
-    // const val CLIENT_ID = BuildConfig.FIXER_ACCESS_KEY
+     const val CLIENT_ID = BuildConfig.FIXER_ACCESS_KEY
 
     // Names to display
     const val DAY_WORD = "Dzień"
@@ -13,4 +15,7 @@ object Constants {
     // TAGs
     const val MAIN_VIEW_MODEL_TAG = "MainViewModel"
     const val EXCHANGE_RATES_FRAGMENT_TAG = "ExchangeRatesFragment"
+
+    // Delays
+    const val GETTING_EXCHANGES_RATES_DAYA_FROM_API_TIME_DELAY = 1000L
 }
